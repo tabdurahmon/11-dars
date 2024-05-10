722ca355c78b5515c2515ed707fd336cf740b857
tabdurahmon <dilsozxonxatamova@gmail.com>
2024-05-10 22:58:15 +0500

    vAZIFA

---
+++ app.js
@@ -0,0 +1,51 @@
+// let sonlar = [1, 2, 3, 4, 5];
+
+// let numbers = sonlar.reverse();
+
+// console.log(numbers);
+
+// n2
+
+// let son = 7;
+
+// if (son == "7") {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// if (son === "7") {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// n3
+// let son = 6;
+
+// for (let i = 1; son >= i; i++) {
+//   console.log([i]);
+// }
+
+//n4
+// let son = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// let juft = son.filter((son) => son % 2 == 0);
+// let toq = son.filter((son) => son % 2 != 0);
+
+// let juftYigindi = juft.reduce((yigindi, son) => yigindi + son);
+// let toqYigindi = toq.reduce((yigindi, son) => yigindi + son);
+
+// console.log(juftYigindi);
+// console.log(toqYigindi);
+
+// n5
+// function matn(str) {
+//     return str.split('!').join('');
+// }
+
+// var inputString = "Bu bir ! misol ! matnidi!";
+// var result = matn(inputString);
+// console.log(result);
+
+// n6
